e397519894d34c9bb1093d14c613b308866b6b45
lxj7976 <1437715223@qq.com>
2018-09-17 19:04:21 +0800

    轮播图初步完成 p和a样式未完成

--- src/views/index/javascripts/modules/banner.js
+++ src/views/index/javascripts/modules/banner.js
@@ -1,2 +1,50 @@
 
 console.log('banner')
+function $id(id){
+    return document.getElementById(id);
+}
+//轮播图效果
+//按钮	
+var oBtnRound=$id("round");
+	var oBtnSingle=$id("single");
+	var oBtnPrev=$id("prev");
+	var oBtnNext=$id("next");
+	var oDivOrder=$id("order");
+	var oDivInfo=$id("info");
+	var oImg=$id("picture");
+	var arrPic=["../../../images/lunbo1.jpg", "../../../images/lunbo2.jpg"];
+	var arrInfo=["贵族童话系列全新上市", "值得拥有的PANDORA经典系列"];
+	var arrTxt=["立即选购", "立即选购"];
+	var index=0;
+	var flag=true;
+	//第一张图片
+	function imgShow(index){
+		oImg.src=arrPic[index];
+		oDivOrder.innerHTML=arrTxt[index];
+		oDivInfo.innerHTML=arrInfo[index];
+	}
+	imgShow(index);
+	
+	//下一张
+	oBtnNext.onclick=function(){
+		index++;
+		if(index==arrPic.length&&flag){
+			index=0;
+		}else if(index==arrPic.length&&!flag){
+			index=arrPic.length-1;
+		}
+		imgShow(index);
+	}
+	
+	//上一页
+	oBtnPrev.onclick=function(){
+		index--;
+		if(index==-1&&flag){
+			index=arrPic.length-1;
+		}else if(index==-1&&!flag){
+			index=0;
+		}
+		imgShow(index);
+	}
+	
+	
