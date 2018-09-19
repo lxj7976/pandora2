ca549491655df45df4678c4f20492c0d6890d983
lxj7976 <1437715223@qq.com>
2018-09-19 14:28:13 +0800

    选项卡功能已实现 样式不完善

--- src/views/index/javascripts/index.js
+++ src/views/index/javascripts/index.js
@@ -13,3 +13,41 @@ $id("i").onclick=function(){
 	
 	alert();
 }*/
+window.onload = function(){
+    new CheckCard().init();
+}
+function CheckCard(){
+    this.alist = document.getElementsByClassName("check");
+    this.clist = document.getElementsByClassName("contener");
+    this.init = function(){
+        for(let i = 0;i < this.alist.length;i ++){
+            this.alist[i].onmouseenter = function(){
+                this.clear();
+                this.show(i);
+            }.bind(this)
+            this.alist[i].onmouseleave = function(){
+                this.clear();
+                this.hide(i);
+            }.bind(this)
+            this.clist[i].onmouseenter = function(){
+                this.clear();
+                this.show(i);
+            }.bind(this)
+            this.clist[i].onmouseleave = function(){
+                this.clear();
+                this.hide(i);
+            }.bind(this)
+        }
+    }
+    this.clear =function(){
+        for(var i = 0;i < this.alist.length;i ++){
+            this.clist[i].style.display = "none";
+        }
+    }
+    this.show = function(index){
+        this.clist[index].style.display = "block";
+    }
+    this.hide = function(index){
+        this.clist[index].style.display = "none";
+    }
+}
