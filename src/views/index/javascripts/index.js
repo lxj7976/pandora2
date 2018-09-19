3833ac4ee119de850eb85fc479b0b5b5bf89ab15
lxj7976 <1437715223@qq.com>
2018-09-19 17:03:24 +0800

    选项卡功能完成 样式基本完成

--- src/views/index/javascripts/index.js
+++ src/views/index/javascripts/index.js
@@ -23,11 +23,16 @@ function CheckCard(){
         for(let i = 0;i < this.alist.length;i ++){
             this.alist[i].onmouseenter = function(){
                 this.clear();
-                this.show(i);
+				this.show(i);
+				this.hasborder(i);
+				this.hasbg(i);
+				
             }.bind(this)
             this.alist[i].onmouseleave = function(){
                 this.clear();
-                this.hide(i);
+				this.hide(i);
+				this.bordernone(i);
+				this.nobg(i);
             }.bind(this)
             this.clist[i].onmouseenter = function(){
                 this.clear();
@@ -49,5 +54,52 @@ function CheckCard(){
     }
     this.hide = function(index){
         this.clist[index].style.display = "none";
-    }
+	}
+	//导航边框样式
+	this.hasborder=function(index){
+		// ../../../images/Pandora_Logo.svg
+		// border-left: 1px solid transparent;
+    	// border-right: 1px solid transparent;
+    	// border-top: 1px solid transparent;
+		this.alist[index].style.borderLeft="1px solid #d2cfd3";
+		this.alist[index].style.borderRight="1px solid #d2cfd3";
+		this.alist[index].style.borderTop="1px solid #d2cfd3";
+	}
+	//去导航边框样式
+	this.bordernone=function(index){
+		// ../../../images/Pandora_Logo.svg
+		// border-left: 1px solid transparent;
+    	// border-right: 1px solid transparent;
+    	// border-top: 1px solid transparent;
+		this.alist[index].style.borderLeft="1px solid transparent";
+		this.alist[index].style.borderRight="1px solid transparent";
+		this.alist[index].style.borderTop="1px solid transparent";
+	}
+	//li背景图
+	this.hasbg=function(index){
+		this.alist[index].style.backgroundImage="url(../../../images/arrow-down.png);"
+		// this.alist[index].style.background="url(../../../images/arrow-down.png) no-repeat center 39px";
+	}
+	//去li背景图
+	this.nobg=function(index){
+		this.alist[index].style.backgroundImage="";
+	}
+}
+function $id(id){
+	return document.getElementById(id);
+}
+
+$id("searchimg").onmouseenter=function(){
+	$id("q").style.display="block";
+	$id("i").style.display="block";
+	this.style.display="none";
+}
+
+
+$id("i").onclick=function(){
+	$id("q").style.display="none";
+	this.style.display="none";
+	setTimeout(function(){
+		$id("searchimg").style.display="block";
+	},500)
 }
