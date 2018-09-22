5c9c4d4e37fbf927b39afe6b9cdc0d5798d396f5
lxj7976 <1437715223@qq.com>
2018-09-22 16:22:30 +0800

    ajax获取成功详情页样式完成

--- src/views/list/javascripts/list.js
+++ src/views/list/javascripts/list.js
@@ -101,71 +101,182 @@ function $id(id){
 }
 
 
-
-//验证邮箱
-var flagEmail=null;
-$id("email").onblur=function(){
-    var Email=$id("email").value;
-    var regEmail = /^[a-z0-9]+@\w+\.\w+$/;
-    if(this.value==""){
-        $id("s7").innerHTML="必填字段";
-        flagEmail=false;
-    }else if(regEmail.test(Email)){
-        $id("s7").innerHTML="";
-        flagEmail=true;
-    }
-}
-//验证密码
-$id("pwd").onblur=function(e){
-    //定义三个正则表示包含数字 字母 特殊字符
-    var _regNum=/\d+/;
-    var _regLetter=/[a-z]+/i;
-    var _regChar=/[^0-9a-z]+/i;
-    var str=this.value;
-    
-    if(this.value==""){
-        $id("s3").innerHTML="必填字段";
-    }
-}
-var loginBtn=document.getElementById("submit");
-var flaglogin=null;
-loginBtn.onclick = function(){
-    //获取cookie
-    var str = getCookie("userlist");
-    var arr = JSON.parse( str );
-    var cookieName = arr[0].username;
-    var cookiePwd = arr[0].userpwd;
-    //获取用户输入的用户名和密码 :
-    var tname = $id("email").value;
-    var tpwd = $id("pwd").value;
-    console.log(cookieName);
-    console.log(cookiePwd);
-
-    if( cookieName == tname && cookiePwd == tpwd ){
-        alert("登录成功");
-        location.href="page.html";//无法实现跳转
-    }else{
-        alert("密码或账户无效");
-        $id("s4").innerHTML="密码或账户无效";
-    }
-}
+// //左边选项
+// //委托  添加 
+// $(".left").delegate(".tu","click",function(){
+// 	alert();
+// 	$(".list").css("display","block");	
+	
+// })
 
 
 
-function getCookie(key){
-    var str = document.cookie;
-    if( str ){ //如果cookie存在  根据key取对应的值
-        str = str.replace( /\s/g ,"");//去掉cookie中的空格
-        var arr = str.split(";");//将字符串拆成数组
-        for( var i = 0 ; i < arr.length ; i++ ){
-            var item = arr[i].split("=");
-            if( item[0] == key ){
-                return item[1];
-            }
-        }
-        //循环结束后 如果没有找到对应的key   返回""
-        return "";
-    }
-    //cookie 不存在  返回""
-    return "";
-}
+//渲染数据
+// $(function () {
+// 	init();
+// 	function init() { // 初始化函数
+// 		get_list_goods(render_goods_in_list); // 获取数据
+// 		// add_event_listener(); // 给元素们绑定事件
+// 	}
+// 	function get_list_goods(callback) { // 获取列表中商品数据的
+// 		$.ajax({
+// 			url: 'data.json',
+			
+// 			success (res) {
+// 				if (res.code == 200) {
+// 					callback(res.data); // 将数据交由回调函数处理
+// 				} else {
+// 					alert('接口获取失败');
+// 				}
+// 			},
+// 			error () {
+// 				alert('接口获取失败');
+// 			}
+// 		})
+// 	}
+
+// 	function render_goods_in_list(data) { // 能接收到需要渲染的数据，作用是渲染数据
+// 		var str = ''
+// 		data.forEach(item => {
+// 			str += good_item_template(item);
+// 		});
+// 		$('.good-list').html(str);
+// 	}
+
+// 	function good_item_template(good) { // 接收到对应的商品数据，返回拼接好的模板
+// 		let { good_id, good_type, good_name, good_price, good_image } = good;
+// 		return `
+// 		<tr>
+// 			<td>
+// 				<div class="content" data="${good_id}">
+// 					<p class="new-product">${good_type}</p>
+// 					<img src="${good_image}" alt="">
+// 					<p class="name">${good_name}</p>
+// 					<p class="price">${good_price}</p>
+// 				</div>
+// 			</td>
+// 			<td>
+// 				<div class="content" data="${good_id}">
+// 					<p class="new-product">${good_type}</p>
+// 					<img src="${good_image}" alt="">
+// 					<p class="name">${good_name}</p>
+// 					<p class="price">${good_price}</p>
+// 				</div>
+// 			</td>
+// 			<td>
+// 				<div class="content" data="${good_id}">
+// 					<p class="new-product">${good_type}</p>
+// 					<img src="${good_image}" alt="">
+// 					<p class="name">${good_name}</p>
+// 					<p class="price">${good_price}</p>
+// 				</div>
+// 			</td>
+// 		</tr>
+// 		`
+// 	}
+
+
+//渲染数据
+$(function () {
+	init();
+	function init() { // 初始化函数
+		get_list_goods(render_goods_in_list); // 获取数据
+		// add_event_listener(); // 给元素们绑定事件
+	}
+	function get_list_goods(callback) { // 获取列表中商品数据的
+		$.ajax({
+			url: 'data.json',
+			
+			success (res) {
+				if (res.code == 200) {
+					callback(res.data); // 将数据交由回调函数处理
+				} else {
+					alert('接口获取失败');
+				}
+			},
+			error () {
+				alert('接口获取失败');
+			}
+		})
+	}
+
+	function render_goods_in_list(data) { // 能接收到需要渲染的数据，作用是渲染数据
+		var str = ''
+		data.forEach(item => {
+			str += good_item_template(item);
+		});
+		$('.good-list').html(str);
+	}
+
+	function good_item_template(good) { // 接收到对应的商品数据，返回拼接好的模板
+		let { good_id, good_type, good_name, good_price, good_image } = good;
+		return `
+			<li class="content">
+			<p class="new-product">${good_type}</p>
+				<img src="${good_image}" alt="">
+				<p class="name">${good_name}</p>
+				<p class="price">${good_price}</p>
+				<a id="quick" href="" style="display:none;" title="闪亮泪滴耳钉">快速查看</a>
+			</li>
+		`
+	}
+
+	// function add_event_listener() { // 在这里为大部分dom绑定事件，并且采用事件委托的方式为未来元素绑定事件
+		
+	// 	$('.good-list').on('click', '.add-btn', function () { // 增加数量的按钮动作
+	// 		let num_input = $(this).siblings('.number');
+	// 		let value = num_input.val();
+	// 		num_input.val(++value);
+	// 	})
+	// 	$('.good-list').on('click', '.reduce-btn', function () { // 减少数量的按钮动作
+	// 		let num_input = $(this).siblings('.number');
+	// 		let value = num_input.val();
+	// 		if ( value <= 1 ) return false;
+	// 		num_input.val(--value);
+	// 	})
+
+	// 	$('.good-list').on('click', '.buy-btn', function () { // 加入购物车按钮的动作
+	// 		// 判断是否登录
+	// 		check_is_login( () => {
+	// 			// 加入购物车
+	// 			let good = get_good_by_btn.call(this); // 对应的商品
+	// 			car_util.save_good_in_car(good, {
+	// 				success () {
+	// 					alert('加入成功')
+	// 				}
+	// 			}); // 加入购物车
+	// 		})
+	// 	})
+	// }
+
+	// function get_good_by_btn() { // 获取到加入购物车按钮所对应的商品信息
+	// 	let $item = $(this).parents('.good-item')
+	// 	return {
+	// 		good_id: $item.data('id'),
+	// 		good_name: $item.find('.name').html(),
+	// 		good_price: $item.find('.price').html(),
+	// 		good_num: $item.find('.number').val() * 1,
+	// 		good_image: $item.find('.good-img img').prop('src')
+	// 	}
+	// }
+
+	/*
+	*   check_is_login 检查是否登录
+	*   @params success function required 判断登录了之后执行
+	*   @params fail function  判断没有登录了之后执行
+	*/
+	// function check_is_login(success, fail) {
+	// 	let user_info = $.cookie('user_info');
+	// 	// if ( user_info ) { // 登录了
+	// 	if ( true ) { // 因为没有登录功能，所以默认已经登录了
+	// 		success(user_info);
+	// 	}else { // 没有登录
+	// 		if ( fail ) {
+	// 			fail();
+	// 		}else {
+	// 			alert('请登陆后操作')
+	// 		}
+	// 	}
+	// }
+})
+
