a3e1724ffde8c65823a24b4406cb6be4d77c97b4
lxj7976 <1437715223@qq.com>
2018-09-23 16:08:25 +0800

    放大镜功能完成 样式基本完成

--- src/views/show/javascripts/show.js
+++ src/views/show/javascripts/show.js
@@ -88,3 +88,46 @@ $id("i").onclick=function(){
 		$id("searchimg").style.display="block";
 	},500)
 }
+// $("#mm").siblings().css("border","#006699 3px solid") 
+	//鼠标移入到底部小图时，显示对应的中图和大图
+	$("#bottom li").mouseover(function(){
+		var index = $(this).index();
+		$("#small img").eq(index).show().siblings("img").hide();
+		$("#big img").eq(index).show().siblings().hide();
+	})
+	//鼠标移入移出small盒子  显示或隐藏大图显示区big  和 mask
+	$("#small").on({
+		"mouseover":function(){
+			$("#big").show();
+			$("#mask").show();
+		},
+		"mouseout":function(){
+			$("#big").hide();
+			$("#mask").hide();
+		},
+		"mousemove":function(evt){
+			var e = evt || event;
+			var x = e.clientX - $("#small").offset().left - $("#mask").width()/2;
+			var y = e.clientY - $("#small").offset().top - $("#mask").height()/2;
+			
+			var mx = $("#small").width()  - $("#mask").width();
+			var my = $("#small").height()  - $("#mask").height();
+			
+			//边界处理
+			x = x<=0 ? 0 : x>=mx ? mx : x;
+			y = y<=0 ? 0 : y>=my ? my : y;
+			
+			//大图宽度/小图宽度 = 大图偏移 / mask的偏移
+			var bigImageX = -x * $(".bigImage").width() / $("#small").width();
+			var bigImageY = -y * $(".bigImage").height() / $("#small").height();
+			$("#mask").css({
+				"left":x + "px",
+				"top":y+"px"
+			})
+			
+			$(".bigImage").css({
+				"left" : bigImageX+"px",
+				"top" : bigImageY+"px"
+			})
+		}
+	})
