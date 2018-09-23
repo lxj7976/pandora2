3276403e4cc3c782c72dbc40bf226af768ba3c11
lxj7976 <1437715223@qq.com>
2018-09-23 17:39:23 +0800

    楼梯完成

--- src/views/index/javascripts/index.js
+++ src/views/index/javascripts/index.js
@@ -103,3 +103,57 @@ $id("i").onclick=function(){
 		$id("searchimg").style.display="block";
 	},500)
 }
+
+
+//楼梯
+var flag = true;//开关边框控制滚动条是否被触发  假设值为true时  可以触发滚动条
+	//点击左侧楼层号  除了 top
+	$("#LoutiNav li:not(:last)").click(function(){
+		flag = false;
+		//当前点击的span的文字红色  其余默认
+		$(this).find("span")
+			   .addClass("active")
+			   .end()
+			   .siblings()
+			   .find("span")
+			   .removeClass("active");
+			   
+		//根据当前操作的楼层号的下标  找到对应楼层距离文档顶部的 距离 
+		var topHeight = $(".Louti").eq( $(this).index() ).offset().top;
+		
+		//设置滚动条滚走的距离 为  topHeight
+		$("body,html").stop().animate({"scrollTop":topHeight},1000,function(){
+			flag = true; //点击事件动画完成后，将开关变量改变成true，让scroll滚动条事件可以继续执行
+		})
+	})
+	
+	//点击楼层号的最后一个按钮   滚动条回到顶部
+	$(".last").click(function(){
+		flag = false;
+		$("body,html").animate({"scrollTop":0},1000,function(){
+			flag = true;
+		});
+		$("#LoutiNav li:not(:last)").removeClass("active");
+	})
+	//操作滚动条
+	$(window).scroll(function(){
+		if(flag){
+			
+			//获取滚动条滚走距离
+			var sTop = $(document).scrollTop();
+			
+			var $floor = $(".Louti").filter(function(){
+				return Math.abs( $(this).offset().top - sTop )   < $(this).height()/2;
+			})
+	
+			//根据楼层的索引  找到对应的楼层号   设置样式
+			$("#LoutiNav li").eq( $floor.index() )
+							 .find("span")
+							 .addClass("active")
+							 .end()
+							 .siblings()
+							 .find("span")
+							 .removeClass("active");
+							 
+		}
+	})
